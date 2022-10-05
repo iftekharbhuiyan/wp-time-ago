5f986d1b7805f47162c756e52d25f0f6e70215e4
iftekharbhuiyan <ib.nadim@gmail.com>
2022-10-05 14:22:06 -0400

    Conditional Update

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 // post published time in unix format within the loop
-echo time_ago_format(get_the_time('U'));
+if (function_exists('time_ago_format')) {
+	echo time_ago_format(get_the_time('U'));
+}
 
 // post modified time in unix format within the loop
-echo time_ago_format(get_the_modified_date('U'));
+if (function_exists('time_ago_format')) {
+	echo time_ago_format(get_the_modified_date('U'));
+}
 
 // comment time in unix format within the loop
-echo time_ago_format(get_comment_time('U'));
+if (function_exists('time_ago_format')) {
+	echo time_ago_format(get_comment_time('U'));
+}
 ?>
