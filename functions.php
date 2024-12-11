0338a280f266686b39488e6dd7f5d410e7478466
iftekharbhuiyan <ibhuiyan@mail.com>
2024-12-10 23:33:34 -0500

    Formatting update

--- functions.php
+++ functions.php
@@ -3,12 +3,12 @@
  * display published and modifed time
  * in "time ago" format within the loop
  * @param date: unix format date & time
- * @param str: time ago in string
+ * @param string: time ago in string
  */
 function time_ago_format($unix_time) {
-	if (!empty($unix_time)) {
-		$timeago = human_time_diff($unix_time, current_time('timestamp')).' ago';
-		return $timeago;
-	}
+    if (!empty($unix_time)) {
+        $timeago = human_time_diff($unix_time, current_time('timestamp')).' ago';
+        return $timeago;
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
 // post published time in unix format within the loop
 if (function_exists('time_ago_format')) {
-	echo time_ago_format(get_the_time('U'));
+    echo time_ago_format(get_the_time('U'));
 }
 
 // post modified time in unix format within the loop
 if (function_exists('time_ago_format')) {
-	echo time_ago_format(get_the_modified_date('U'));
+    echo time_ago_format(get_the_modified_date('U'));
 }
-
 // comment time in unix format within the loop
 if (function_exists('time_ago_format')) {
-	echo time_ago_format(get_comment_time('U'));
+    echo time_ago_format(get_comment_time('U'));
 }
 ?>
