6a2dc8c797ab43037aec87186237ae7da6da859d
iftekharbhuiyan <ibhuiyan@mail.com>
2024-12-10 23:36:40 -0500

    Minor update

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 if (function_exists('time_ago_format')) {
     echo time_ago_format(get_the_time('U'));
 }
-
 // post modified time in unix format within the loop
 if (function_exists('time_ago_format')) {
     echo time_ago_format(get_the_modified_date('U'));
